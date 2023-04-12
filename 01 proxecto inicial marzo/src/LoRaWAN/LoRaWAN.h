b1344944f98c9f4b2d28dabffa919217c64faaa3
4m1g0 <dev.4m1g0@gmail.com>
2023-04-12 20:12:52 +0200

    Creado modulo de lorawan

---
+++ 01 proxecto inicial marzo/src/LoRaWAN/LoRaWAN.h
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+
+void LoRaWAN_setup();
+void LoRaWAN_loop();
+
+bool LoRaWAN_enviarGPS(uint8_t* gpsPos);
--- 01 proxecto inicial marzo/src/main.ino
+++ 01 proxecto inicial marzo/src/main.ino
@@ -1,31 +1,24 @@
 #include <Arduino.h>
-#include <TTN_esp32.h>
-
-
-const char* devAddr = "260B510F"; // Change to TTN Device Address
-const char* nwkSKey = "AEBC01BC0DF092F6BB2E9111964EACAB"; // Change to TTN Network Session Key
-const char* appSKey = "CD8C3789C2BAE32E6E431D70D5CDE3E8"; // Change to TTN Application Session Key
-
-TTN_esp32 ttn ;
+#include "LoRaWAN/LoRaWAN.h"
+//#include "GSP/GSP.h"
 
 void setup() {
-
   // put your setup code here, to run once:
-
   Serial.begin(115200);
-  ttn.begin();
-  Serial.println("Inicializando con ABP");
-  ttn.personalize(devAddr, nwkSKey, appSKey);
+  LoRaWAN_setup();
+
+  Serial.println("Inicialization sequence complete...");
 }
 
+unsigned long lastLoraMillis = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Empezando");
-
   uint8_t myByte[] = {2, 3};
 
-  Serial.println("Enviando 1 byte");
-  ttn.sendBytes(myByte, 2);
+  if (millis() - lastLoraMillis > 5000 ) {
+    lastLoraMillis = millis();
+    LoRaWAN_enviarGPS(myByte);
+  }
+  
+  //LoRaWAN_loop();
 
-  delay(5000);
 }
