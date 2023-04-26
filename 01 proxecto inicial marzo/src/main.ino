e885acf92c63cd77d146a7c221248cd4b6b48230
Guillermo Antón <ganton@edu.xunta.gal>
2023-04-26 22:00:42 +0200

    Módulos GPS y Oled, adaptado de ejercicio curso.

--- 01 proxecto inicial marzo/src/main.ino
+++ 01 proxecto inicial marzo/src/main.ino
@@ -1,18 +1,29 @@
 #include <Arduino.h>
 #include "LoRaWAN/LoRaWAN.h"
-//#include "GSP/GSP.h"
+#include "gps/gps.h"
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   LoRaWAN_setup();
+  setupGPS();
 
   Serial.println("Inicialization sequence complete...");
 }
 
 unsigned long lastLoraMillis = 0;
 void loop() {
-  uint8_t myByte[] = {2, 3};
+  uint8_t myByte[9];
+
+    smartGPSdelay(5000);
+  if (checkGPSFix())
+  {
+    buildGPSpacket(myByte);
+    Serial.println("Enviando paquete LoRaWAN");
+    ttn.sendBytes(myByte, sizeof(myByte));
+  }
+  printGPSdata();
+
 
   if (millis() - lastLoraMillis > 5000 ) {
     lastLoraMillis = millis();
